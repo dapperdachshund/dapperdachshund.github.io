b7075d553a528102f350750cc57f7ff2f33344c8
Dan Dietz <danddietz@gmail.com>
2017-04-09 23:05:32 -0400

    added more photos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,6 +13,29 @@ $(document).ready(function() {
 	var background = _.sample([
 		'pine.jpg',
 		'baby.jpg',
+		'bed.jpg',
+		'chair.jpg',
+		'1.jpg',
+		'2.jpg',
+		'3.jpg',
+		'4.jpg',
+		'5.jpg',
+		'6.jpg',
+		'7.jpg',
+		'8.jpg',
+		'9.jpg',
+		'10.jpg',
+		'11.jpg',
+		'12.jpg',
+		'13.jpg',
+		'14.jpg',
+		'15.jpg',
+		'16.jpg',
+		'17.jpg',
+		'18.jpg',
+		'19.jpg',
+		'20.jpg',
+		'21.jpg'
 	]);
 	$.vegas({src:'assets/images/kepler/' + background});
 	$.vegas('overlay', {src:'assets/images/overlays/05.png'});
