ef80eed83cf838da6ee00d6d94f0487470ca45f1
Dan Dietz <danddietz@gmail.com>
2017-04-09 23:18:48 -0400

    more images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -35,7 +35,17 @@ $(document).ready(function() {
 		'18.jpg',
 		'19.jpg',
 		'20.jpg',
-		'21.jpg'
+		'21.jpg',
+		'22.jpg',
+		'23.jpg',
+		'24.jpg',
+		'25.jpg',
+		'26.jpg',
+		'27.jpg',
+		'28.jpg',
+		'29.jpg',
+		'30.jpg',
+		'31.jpg'
 	]);
 	$.vegas({src:'assets/images/kepler/' + background});
 	$.vegas('overlay', {src:'assets/images/overlays/05.png'});
